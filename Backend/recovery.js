b51966eb0b2dccd9cf586511f79705187a0f0850
Nidhekshaa <nknidhekshaa@gmail.com>
2026-02-16 11:44:42 +0530

    port changed

--- Backend/recovery.js
+++ Backend/recovery.js
@@ -12,8 +12,8 @@ const router = express.Router();
 ========================================= */
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for 465
+  port: 587,
+  secure: false, // true for 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
