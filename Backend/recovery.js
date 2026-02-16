f948955ad7397e8478d13cd9268d02e3a13a61d7
Nidhekshaa <nknidhekshaa@gmail.com>
2026-02-16 11:56:50 +0530

    test 3

--- Backend/recovery.js
+++ Backend/recovery.js
@@ -11,9 +11,7 @@ const router = express.Router();
    CREATE MAIL TRANSPORTER ONCE (better perf)
 ========================================= */
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for 465
+  host: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
