9d7ed7c391f7422ef98188adf0c5ba94e1095dd4
Nidhekshaa <nknidhekshaa@gmail.com>
2026-02-18 16:07:50 +0530

    nodemailer changed

--- Backend/recovery.js
+++ Backend/recovery.js
@@ -12,8 +12,8 @@ const router = express.Router();
 ========================================= */
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
