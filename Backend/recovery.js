4fafd6dcd86c5f87e82249f6962d9b6507ea8060
Nidhekshaa <nknidhekshaa@gmail.com>
2026-02-18 19:24:44 +0530

    port changed

--- Backend/recovery.js
+++ Backend/recovery.js
@@ -12,7 +12,7 @@ const router = express.Router();
 ========================================= */
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: 465,
+  port: 2525,
   secure: true,
   auth: {
     user: process.env.SMTP_USER,
