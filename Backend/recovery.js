4757302008181161609b18eb16498067dd0b148b
Nidhekshaa <nknidhekshaa@gmail.com>
2026-02-18 16:13:53 +0530

    recovery file

--- Backend/recovery.js
+++ Backend/recovery.js
@@ -18,6 +18,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 /* =========================================
